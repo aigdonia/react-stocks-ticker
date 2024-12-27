6ff41248f2ed2deb9ac2cba4ea7be96484aec4d6
Ahmed Gaber <aigdonia@gmail.com>
2024-12-27 23:47:26 +0200

    feat: implemented splash screen

--- src/pages/splash-page.tsx
+++ src/pages/splash-page.tsx
@@ -1,7 +1,11 @@
 export default function SplashPage() {
 	return (
-		<>
-			<h1>Splash Screen</h1>
-		</>
+		<div className="flex flex-col items-center justify-center h-screen gap-4">
+			<img src="./public/nasdaq.svg" alt="Nasdaq Logo" className="w-1/3 m-5"/>
+			<div className="text-slate-800">
+				A Lightweight nasdaq stocks ticker app
+			</div>
+			<div className="text-xl text-slate-700/60">Ahmed Gaber</div>
+		</div>
 	)
 }
