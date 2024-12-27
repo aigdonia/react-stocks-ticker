e892c6d5f74c9f55b4437d7f0d026e9a41c8a465
Ahmed Gaber <aigdonia@gmail.com>
2024-12-27 23:22:04 +0200

    feat: Search Context

---
+++ src/hooks/useSearch.tsx
@@ -0,0 +1,25 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+const SearchContext = createContext({
+	query: "",
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	setQuery: (str: string) => {},
+});
+
+export const SearchProvider = ({ children }: PropsWithChildren ) => {
+	const [query, setQuery] = useState("");
+	return (
+		<SearchContext.Provider value={{ query, setQuery }}>
+			{children}
+		</SearchContext.Provider>
+	);
+};
+
+export const useSearch = () => {
+	const context = useContext(SearchContext);
+
+	if (!context) {
+		throw new Error("useSearch must be used within a SearchProvider");
+	}
+	return context;
+};
