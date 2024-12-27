7ec95efdcac0b5cc30e508c8d1f669c9907f56a6
Ahmed Gaber <aigdonia@gmail.com>
2024-12-27 23:48:08 +0200

    feat: added spalsh, simulate app loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Toaster } from 'sonner';
 import { ExplorePage } from "./pages/explore-page"
 import { SearchProvider } from "./hooks/useSearch"
+import SplashPage from './pages/splash-page';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [appLoading, setAppLoading] = useState(true)
+
+	// Simulate app loading
+	useEffect(() => {
+		const timeoutHandler = setTimeout(() => {
+
+			setAppLoading(false)
+		}, 1000)
+
+		return () => clearTimeout(timeoutHandler)
+	}, [])
+
 	return (
 		<SearchProvider>
 			<Toaster />
-			<ExplorePage />
+			{appLoading && <SplashPage />}
+			{!appLoading && <ExplorePage />}
 		</SearchProvider>
 	)
 }
